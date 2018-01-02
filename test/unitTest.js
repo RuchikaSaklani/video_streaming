f818e602dedb99949d201eb84a040730dfdc9e44
Ruchika <ruchikasaklani07@gmail.com>
2018-01-02 14:54:38 +0530

    Create unitTest.js

---
+++ test/unitTest.js
@@ -0,0 +1,14 @@
+let test = require('../server/routes/users.js');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+
+
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(["upload", "file", "azure"].indexOf(4), -1);
+    });
+  });
+});

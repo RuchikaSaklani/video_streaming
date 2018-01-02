cf4a479c7560fca5e59b983be49a01376d78a5e3
Ruchika <ruchikasaklani07@gmail.com>
2018-01-02 15:39:49 +0530

    Update unitTest.js

--- test/unitTest.js
+++ test/unitTest.js
@@ -1,14 +1,35 @@
-let test = require('../server/routes/users.js');
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let should = chai.should();
+let app = require('../server/routes/users.js');
+var chai = require('chai'), chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
+let expect = chai.expect();
 
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(["upload", "file", "azure"].indexOf(4), -1);
+describe('File upload', function(){
+it('should be able to get the files', function(done) {
+  chai.request(app)
+    .get('/upload')
+      done();
     });
+
+it('should be able to find all files', function(done) { 
+  chai.request('app')
+  .get('/upload')
+  .end(function(err, res) {
+    expect(res).to.have.status(200);
+                                
   });
+  done();
+}) ;
+
+it('it should be able to return array', (done) => {
+    chai.request('app')
+.post('/add')
+.end((err, res) => {
+expect(res).to.be.an.instanceof(Array);
+           
+            });
+      done();
+      });
 });
+
+
